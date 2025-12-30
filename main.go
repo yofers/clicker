909e673cdb4cbebbc00184e91a47191c32fca10f
mahiro <tanyunhui86@163.com>
2025-12-30 17:22:24 +0800

    docs: 将应用标题从中文改为英文

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "点击器",
+		Title:  "Clicker",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
